629a70af75519a91b45cdcf6e3882edec3f821bb
dantser <dantser@gmail.com>
2017-05-30 09:26:22 +0300

    add js toburger

---
+++ app/blocks/burger/burger.js
@@ -0,0 +1,10 @@
+export default () => {
+  const burgerClass = '.js-burger';
+  const activeBurgerClass = 'burger_active';
+
+  $(burgerClass).on('click', (e) => {
+    const burger = $(burgerClass);
+    e.preventDefault();
+    burger.toggleClass(activeBurgerClass);
+  });
+}
