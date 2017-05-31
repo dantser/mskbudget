349af452418615a749191a15181728af0f90d772
dantser <dantser@gmail.com>
2017-05-31 16:38:51 +0300

    quiz-question

---
+++ app/components/quiz-question/quiz-question.js
@@ -0,0 +1,12 @@
+import $ from 'jquery';
+
+export default () => {
+  const TRIGGER = $('.quiz-hint-trigger');
+  const HINT = $('.quiz-hint');
+  const CLOSE = $('.d-si-content__quiz-hint-close');
+  const TRIGGERHIDE_CLASS = 'hide';
+  TRIGGER.on('click', () => {
+
+  })
+
+};
