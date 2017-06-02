6adeae2c771da350f0f1bde06b753e172a8ebc20
dantser <dantser@gmail.com>
2017-06-02 10:30:31 +0300

    fix

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,10 @@
 import $ from 'jquery';
 import selectbox from '../blocks/selectbox/selectbox';
+import questions from '../components/questions/questions';
+import burger from '../blocks/burger/burger';
 
 $(() => {
   selectbox();
+  questions();
+  burger();
 });
