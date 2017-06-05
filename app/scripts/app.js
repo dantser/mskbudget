49a8fb719c607e339c8e41a55967010b425d4a54
dantser <dantser@gmail.com>
2017-06-05 13:44:05 +0300

    regions fixes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,6 +4,7 @@ import question from '../blocks/question/question';
 import questions from '../components/questions/questions';
 import burger from '../blocks/burger/burger';
 import lkNav from '../blocks/lk-nav/lkNav';
+import regions from '../components/regions/regions';
 
 $(() => {
   selectbox();
@@ -11,4 +12,5 @@ $(() => {
   questions();
   burger();
   lkNav();
+  regions();
 });
