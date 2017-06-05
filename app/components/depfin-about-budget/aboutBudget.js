80dfceb00f89ff39a30ca9f4262d092d79b65cbb
dantser <dantser@gmail.com>
2017-06-05 17:14:55 +0300

    about budget

---
+++ app/components/depfin-about-budget/aboutBudget.js
@@ -0,0 +1,16 @@
+// import $ from 'jquery';
+//
+// export default () => {
+//   const DIVISIONS = $('.whatIsBudget-expenditures_divisions');
+//   const TYPES = $('.whatIsBudget-expenditures_types');
+//   const PROGRAMS = $('.whatIsBudget-expenditures_programs');
+//   const CLASSIFY = $('.whatIsBudget-expenditures__classification');
+//   const TITLE = $('.whatIsBudget-expenditures__titleWrapper');
+//
+//   if (TYPES.is(':visible')) {
+//     CLASSIFY.css('transform', 'translateX(-29%)');
+//   } else if (PROGRAMS.is(':visible')) {
+//     CLASSIFY.css('transform', 'translateX(-55%)');
+//     TITLE.css('transform', 'translateX(-55%)');
+//   }
+// }
