f30b134d3d0ff268507c0d32c1b0b0cb736ab5ed
dantser <dantser@gmail.com>
2017-06-11 16:25:46 +0300

    sources

--- app/components/budget_moscow_sources/budget_moscow_sources.js
+++ app/components/budget_moscow_sources/budget_moscow_sources.js
@@ -3,20 +3,35 @@ import $ from 'jquery';
 export default () => {
   const ARR_RIGHT = $('.ar-right');
   const ARR_LEFT = $('.ar-left');
+  const SWITCH = 'arr-switchclass';
 
   ARR_RIGHT.on('click', (e) => {
     e.preventDefault();
     if ($('.table__data:last-child').is(':visible')) {
       return;
     }
-    $('.table__data:visible').css('display', 'none').next().css('display', 'table-cell');
+
+    if ($(window).width() < 700) {
+        $('.table__data:visible').hide().next().css('display', 'block');
+    }
+    else {
+      $('.table__data:visible').hide().next().css('display', 'table-cell');
+    }
+
   });
 
   ARR_LEFT.on('click', (e) => {
     e.preventDefault();
-    if ($('.d-smr__chart-col:visible[data-char = revenue]').is(':visible')) {
+    if ($('.table__data:nth-child(2)').is(':visible')) {
       return;
     }
-    $('.d-smr__chart-col:visible[data-char]').css('display', 'none').prev().css('display', 'table-cell');
+
+    if ($(window).width() < 700) {
+      $('.table__data:visible').hide().prev().css('display', 'block');
+    }
+    else {
+      $('.table__data:visible').hide().prev().css('display', 'table-cell');
+    }
+
   });
 };
