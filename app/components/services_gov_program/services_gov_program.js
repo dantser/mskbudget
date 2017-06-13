4095c9328d93eb6ebe1c18e7f140b1363b419a67
dantser <dantser@gmail.com>
2017-06-13 18:33:28 +0300

    gov_program fix

--- app/components/services_gov_program/services_gov_program.js
+++ app/components/services_gov_program/services_gov_program.js
@@ -3,10 +3,23 @@ import $ from 'jquery';
 export default () => {
   const SLIDE_TEXT = $('.gov-program__tasks .text:nth-child(n+3)');
   const SHOWHIDE = $('.js-showhide');
+  const DROPDOWN = '.dropdown';
+  const DROPDOWN_BTN = $('.dropdown__btn');
+
 
   if (!SHOWHIDE || !SLIDE_TEXT) {
     return;
   }
+
+  DROPDOWN_BTN.each( function() {
+    const EL = $(this);
+    const DROPDOWN_ACTIVE_CLASS = 'dropdown_active';
+    EL.on('click', (e) => {
+      e.preventDefault();
+      DROPDOWN.removeClass(DROPDOWN_ACTIVE_CLASS);
+      EL.parent(DROPDOWN).toggleClass(DROPDOWN_ACTIVE_CLASS);
+    });
+  });
 
   SHOWHIDE.each( function () { // eslint-disable-line
     const EL = $(this);
