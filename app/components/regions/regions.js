d677a21dc22d57b58f31d5636ba184bfeb4e3ec7
dantser <dantser@gmail.com>
2017-06-15 18:35:53 +0300

    regions fix

--- app/components/regions/regions.js
+++ app/components/regions/regions.js
@@ -3,6 +3,7 @@ import $ from 'jquery';
 export default () => {
   const ARR_RIGHT = $('.ar-right');
   const ARR_LEFT = $('.ar-left');
+  const HIDE_COL = $('.js-hide-col');
 
   ARR_RIGHT.on('click', (e) => {
     e.preventDefault();
@@ -18,5 +19,16 @@ export default () => {
       return;
     }
     $('.d-smr__chart-col:visible[data-char]').css('display', 'none').prev().css('display', 'table-cell');
+  });
+
+  HIDE_COL.each( function () {
+    const EL = $(this);
+    EL.on('click', (e) => {
+      e.preventDefault();
+      const ELTEXT = EL.text();
+      const DATA = EL.parents('.d-smr__chart-col').attr('data-char');
+      $("[data-char='" +DATA+ "']").not(EL.parents('.d-smr__chart-col')).children().toggle();
+      EL.text(ELTEXT === 'Скрыть колонку' ? 'Показать колонку' : 'Скрыть колонку');
+    })
   });
 };
