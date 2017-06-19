4030b7293c4705233c6986050509d96fcf960e0d
dantser <dantser@gmail.com>
2017-06-19 21:00:34 +0300

    analitys income

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,6 +9,7 @@ import searchForm from '../components/searchForm/searchForm';
 import sources from '../components/budget_moscow_sources/budget_moscow_sources';
 import govProgram from '../components/services_gov_program/services_gov_program';
 import analyticsMain from '../components/depfin-analytics-main/depfin-analytics-main';
+import legend from '../blocks/legend/legend';
 
 $(() => {
   selectbox();
@@ -21,4 +22,5 @@ $(() => {
   sources();
   govProgram();
   analyticsMain();
+  legend();
 });
