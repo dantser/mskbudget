2c866e39b285c53c51a6fcf09198ac941b1bb651
Ilya <rewasd91@mail.ru>
2017-06-22 11:19:27 +0300

    media-main

--- app/components/media-main/mediaMain.js
+++ app/components/media-main/mediaMain.js
@@ -27,5 +27,27 @@ export default () => {
       $('.media-main__materials[data-filter="'+mfilter+'"]').addClass('active');
     });
     
+    if ($(".media-main__datepicker" ).length) {
+		$( ".media-main__datepicker" ).datepicker({
+            changeMonth: true,
+            changeYear: true,
+			closeText: 'Закрыть',
+			prevText: '&#x3c;Пред',
+			nextText: 'След&#x3e;',
+			currentText: 'Сегодня',
+			monthNames: ['Январь','Февраль','Март','Апрель','Май','Июнь',
+			'Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
+			monthNamesShort: ['Январь','Февраль','Март','Апрель','Май','Июнь',
+			'Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
+			dayNames: ['воскресенье','понедельник','вторник','среда','четверг','пятница','суббота'],
+			dayNamesShort: ['вск','пнд','втр','срд','чтв','птн','сбт'],
+			dayNamesMin: ['вс','пн','вт','ср','чт','пт','сб'],
+			dateFormat: 'dd.mm.yy',
+			firstDay: 1,
+			isRTL: false,
+            showOtherMonths: true
+        });
+    }
+    
   });
 }
