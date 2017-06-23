79251edbe04816cf0d3b8ee84d3b547855fa407e
Sergei Kornienko <qluqua@gmail.com>
2017-06-23 18:48:42 +0300

    fast fix

--- app/blocks/range/range.js
+++ app/blocks/range/range.js
@@ -3,18 +3,20 @@ import noUiSlider from 'nouislider';
 export default function range() {
   var slider = document.getElementById('slider');
 
-  noUiSlider.create(slider, {
-    start: [ 30 ],
-    range: {
-      'min': 0,
-      'max': 100
-    },
-    // connect: [false, true],
-    tooltips: [true],
-    pips: {
-      mode: 'values',
-      values: [20, 80],
-      density: 4
-    }
-  });
+  if (slider) {
+    noUiSlider.create(slider, {
+      start: [ 30 ],
+      range: {
+        'min': 0,
+        'max': 100
+      },
+      // connect: [false, true],
+      tooltips: [true],
+      pips: {
+        mode: 'values',
+        values: [20, 80],
+        density: 4
+      }
+    });
+  }
 }
