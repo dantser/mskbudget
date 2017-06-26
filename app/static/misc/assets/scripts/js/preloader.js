5631c49114df0fb2c6ec0aa8cc82f49f4e7cca12
dantser <dantser@gmail.com>
2017-06-26 17:32:04 +0300

    fixes

--- app/static/misc/assets/scripts/js/preloader.js
+++ app/static/misc/assets/scripts/js/preloader.js
@@ -2,14 +2,20 @@
 $(document).ready(function() {
   var preloader = $('#preloader');
   var sections = $('.sections_main-page');
+  var msie = ua.indexOf("MSIE ");
+  var edge = ua.indexOf('Edge/');
+  if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./) || edge > 0 )   {
+    preloader.fadeOut('slow', function() {});
+    $('.sections').removeClass('sections_main-page');
+  };
+
   window.onload = function () {
     if(!preloader || !sections) {
       return
-    }
+    };
     setTimeout(function() {
       preloader.fadeOut('slow', function() {});
       $('.sections').removeClass('sections_main-page');
     }, 2000);
   }
-
 });
