9bd7f17267617aec97724a4d3c8b58ad3c5645bd
dantser <dantser@gmail.com>
2017-06-26 15:26:05 +0300

    fixes

--- app/static/misc/assets/scripts/js/preloader.js
+++ app/static/misc/assets/scripts/js/preloader.js
@@ -1,12 +1,15 @@
 "use strict";
 $(document).ready(function() {
   var preloader = $('#preloader');
+  var sections = $('.sections_main-page');
   window.onload = function () {
-    if(!preloader) {
+    if(!preloader || !sections) {
       return
     }
     setTimeout(function() {
       preloader.fadeOut('slow', function() {});
+      $('.sections').removeClass('sections_main-page');
     }, 2000);
   }
+
 });
