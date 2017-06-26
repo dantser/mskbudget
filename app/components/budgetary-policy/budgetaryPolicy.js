d768e2cafa48fad14b536b7a3915f3295c58a9ab
Ilya <rewasd91@mail.ru>
2017-06-26 12:20:21 +0300

    carousel fixes

---
+++ app/components/budgetary-policy/budgetaryPolicy.js
@@ -0,0 +1,39 @@
+import $ from 'jquery';
+
+export default () => {
+  
+  $(document).ready(function(){
+    $('.mobile-carousel .d-slide__more').click(function(){
+      $(this).prev().addClass('active');
+      $(this).addClass('active');
+    });
+  });
+  
+  function checkListHeight() {
+    $('.mobile-carousel .d-slide__body_list').each(function(){
+      var listHeight = 0;
+      var heightRule = $(this).outerHeight();
+      $(this).children().each(function(){
+        var elHeight = $(this).outerHeight();
+        listHeight += elHeight;
+      });
+      if (listHeight < heightRule) {
+        $(this).next().addClass('active');
+      } else {
+        $(this).removeClass('active');
+        $(this).next().removeClass('active');
+      }
+    });
+  }
+  
+  $(document).ready(function(){
+    checkListHeight();
+    var winWidth = $(window).outerWidth();
+    $(window).resize(function(){
+      if ($(window).width() != winWidth && $('.mobile-carousel').length) {
+        location.reload();
+      }
+    });
+  });
+
+}
