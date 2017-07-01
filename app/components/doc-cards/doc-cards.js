4e7d245d7cbc7fba20ecbf230d9abd5b0d48be04
dantser <dantser@gmail.com>
2017-07-01 15:25:01 +0300

    dropdown

--- app/components/doc-cards/doc-cards.js
+++ app/components/doc-cards/doc-cards.js
@@ -6,23 +6,6 @@ export default () => {
   const TOP_BUTTON = $('.js-button-top');
   const NAV = $('.doc-cards__nav');
 
-  const docSlider = new Swiper('.docs-container', {
-        spaceBetween: 10,
-        slidesPerView: 4,
-        nextButton: '.ar-next',
-        prevButton: '.ar-prev',
-        breakpoints: {
-          992: {
-            slidesPerView: 3,
-          },
-          640: {
-            slidesPerView: 2,
-          },
-          500: {
-            slidesPerView: 1,
-          }
-    }});
-
   DIR.each( function () { // eslint-disable-line
     const EL = $(this);
     // const ACTIVE_CLASS = 'graphic__data_active';
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -32,6 +32,7 @@ import modal from '../blocks/modal/modal';
 import budget_moscow_gov_program from '../components/budget_moscow_gov_program/budget_moscow_gov_program';
 import analitycGraphics from '../components/analityc-graphics/analityc-graphics'
 import docCards from '../components/doc-cards/doc-cards';
+import dropdown from '../blocks/dropdown-block/dropdown';
 
 $(() => {
 
@@ -78,4 +79,5 @@ $(() => {
   budget_moscow_gov_program();
   analitycGraphics();
   docCards();
+  dropdown();
 });
