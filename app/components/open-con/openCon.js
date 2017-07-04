bacdc126d0c4c417271993cca6a0894875d2c72e
dantser <dantser@gmail.com>
2017-07-04 17:03:56 +0300

    fixes

--- app/components/open-con/openCon.js
+++ app/components/open-con/openCon.js
@@ -3,13 +3,13 @@ import $ from 'jquery';
 export default () => {
   const TABLINK = $('.open-con .button-light');
 
-  TABLINK.each( function () { // eslint-disable-line
+  TABLINK.each( function () {
     const EL = $(this);
     const ACTIVE_CLASS = 'button-light--fill';
     EL.on('click', (e) => {
       e.preventDefault();
       TABLINK.removeClass(ACTIVE_CLASS);
-      EL.toggleClass(ACTIVE_CLASS);
+      EL.addClass(ACTIVE_CLASS);
     })
   })
 
