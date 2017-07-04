3490ec1482f347b5c190bc86da47bd2fce4c8237
dantser <dantser@gmail.com>
2017-07-04 10:59:29 +0300

    budget_moscow_gov_program

--- app/components/budget_moscow_gov_program/budget_moscow_gov_program.js
+++ app/components/budget_moscow_gov_program/budget_moscow_gov_program.js
@@ -12,4 +12,28 @@ export default () => {
       EL.parent().addClass(ACTIVE_CLASS);
     })
   })
+
+  const SWITCHER = $('.analityc-widget_moscow-gov-program .analityc-control-switcher_large');
+	const GRAPHICDONE = $('.analityc-widget_moscow-gov-program .analityc-widget-moscow-gov-program_done');
+	const GRAPHICSTRUCTURE = $('.analityc-widget_moscow-gov-program .analityc-widget-moscow-gov-program_structure');
+  console.log(GRAPHICDONE);
+  SWITCHER.on('click', 'a', function(e) {
+		e.preventDefault();
+
+		SWITCHER.children('a').each(function(id, item) {
+			$(item).removeClass('active');
+		});
+
+		$(this).addClass('active');
+
+		if ($(this).attr('data-type') == 'gp') {
+			GRAPHICDONE.addClass('_active');
+
+			GRAPHICSTRUCTURE.removeClass('_active');
+		}
+		if ($(this).attr('data-type') == 'structure') {
+			GRAPHICDONE.removeClass('_active');
+			GRAPHICSTRUCTURE.addClass('_active');
+		}
+	});
 }
