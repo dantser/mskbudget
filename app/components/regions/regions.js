672f1aa334b5062e6eef7852278c71f1b62037ed
dantser <dantser@gmail.com>
2017-07-06 18:31:39 +0300

    fixes

--- app/components/regions/regions.js
+++ app/components/regions/regions.js
@@ -4,6 +4,7 @@ export default () => {
   const ARR_RIGHT = $('.ar-right');
   const ARR_LEFT = $('.ar-left');
   const HIDE_COL = $('.js-hide-col');
+  const SORT = $('.sort_desc');
 
   ARR_RIGHT.on('click', (e) => {
     e.preventDefault();
@@ -31,4 +32,20 @@ export default () => {
       EL.text(ELTEXT === 'Скрыть колонку' ? 'Показать колонку' : 'Скрыть колонку');
     })
   });
+
+  SORT.each(function () {
+    const EL = $(this);
+    EL.on('click', (e) => {
+      e.preventDefault();
+      const ELTEXT = EL.text();
+      EL.text(ELTEXT === 'По убыванию' ? 'По возрастанию' : 'По убыванию');
+      EL.toggleClass('sort_desc').toggleClass('sort_desc-top');
+    })
+  })
+
+  $('#a').on('click', function() {
+    const checkBoxes = $("input[name=a\\[\\]]");
+    checkBoxes.prop("checked", !checkBoxes.prop("checked"));
+  })
+
 };
