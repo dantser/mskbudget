1be39a61b695280f3eddc652e8f6a98c2d687f0c
Sergei Kornienko <qluqua@gmail.com>
2017-07-17 20:16:59 +0300

    fix: соц. кнопки в регистрации

--- app/blocks/menu/menu.js
+++ app/blocks/menu/menu.js
@@ -1,30 +1,30 @@
 import $ from 'jquery';
 
 export default () => {
-  if ( $('#menu .menu__link.has-dropdown').length ) {
+  if ($('#menu .menu__link.has-dropdown').length) {
     function closeSubMenu() {
       $('#menu .menu__link.has-dropdown').removeClass('dropdown-is-active');
     }
 
     $('#menu .menu__link.has-dropdown').on('click', function(event) {
-      if ( $(window).width() >= 1024 ) {
+      if ($(window).width() >= 1024) {
         event.preventDefault();
 
-        if ( $(this).hasClass('dropdown-is-active') ) {
+        if ($(this).hasClass('dropdown-is-active')) {
           closeSubMenu();
         } else {
           closeSubMenu();
           $(this).addClass('dropdown-is-active');
         }
       }
-    }).on('focusout', function () {
+    }).on('blur', function() {
       closeSubMenu();
     });
 
     $(document).keyup(function(e) {
-        if ( e.keyCode == 27 && $('#menu .menu__link.has-dropdown') ) { // escape key maps to keycode `27`
-          closeSubMenu();
-        }
+      if (e.keyCode == 27 && $('#menu .menu__link.has-dropdown')) { // escape key maps to keycode `27`
+        closeSubMenu();
+      }
     });
   }
 }
