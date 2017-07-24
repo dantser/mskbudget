e3695ef5a6211704c8580614923533eeca21695d
dantser <dantser@gmail.com>
2017-07-24 15:37:37 +0300

    fixes

---
+++ app/components/news-one/news-one.js
@@ -0,0 +1,12 @@
+import $ from 'jquery';
+
+export default () => {
+  $('.alternative-link__title').on('click', function (e) {
+    e.preventDefault();
+    $('.news').removeClass('news_hidden');
+  })
+  $('.news-one__show-more').on('click', function (e) {
+    e.preventDefault();
+    $('.tile__item:nth-child(n+4)').css('display', 'inline-flex');
+  })
+}
--- app/components/news/newsFilter.js
+++ app/components/news/newsFilter.js
@@ -1,22 +1,22 @@
 import $ from 'jquery';
 
 export default () => {
-  
+
   $(document).ready(function(){
-    
+
     $('.news-page__buttons-set a').on('click', function(e){
       e.preventDefault();
       $('.news-page__buttons-set a').removeClass('button-light--fill');
       $(this).addClass('button-light--fill');
       var filter = $(this).data('filter');
-      
+
       if ($(window).width() < 639) {
         var text = $(this).text();
         $('.news-page__buttons-set-selected span:first-child').text(text);
         $('.news-page__buttons-set').slideUp();
         $('.news-page__buttons-set-wrapper').removeClass('active');
       }
-      
+
       $('.news-page .tile__item').removeClass('active');
       setTimeout(function(){
         $('.news-page .tile__item').css('display', 'none');
@@ -36,13 +36,13 @@ export default () => {
         }
       }, 500);
     });
-    
+
     $('.news-page__buttons-set-selected').click(function(e){
       e.stopPropagation();
       $('.news-page__buttons-set-wrapper').addClass('active');
       $('.news-page__buttons-set').slideDown();
     });
-    
+
     $('html').click(function(){
       if ($(window).width() < 639) {
         $('.news-page__buttons-set').slideUp();
@@ -50,4 +50,10 @@ export default () => {
       }
     });
   });
+
+
+  $('.news-page__show-more').on('click', function (e) {
+    e.preventDefault();
+    $('.tile__item:nth-child(n+7)').css('display', 'inline-flex');
+  })
 }
