0936a20a1b5ed4d0d9ee1a2ae76f7dec97a10fa6
qluqua <qluqua@gmail.com>
2017-07-27 19:10:20 +0300

    Главное меню на декстопе теперь работает по ховеру

--- app/blocks/menu/menu.js
+++ app/blocks/menu/menu.js
@@ -1,31 +1,47 @@
 import $ from 'jquery';
 
 export default () => {
-  // выпадающее меню первого уровня
-  if ($('#menu .menu__link.has-dropdown').length) {
+  // главное меню на ДЕКСТОПЕ: экран шире чем 1024px
+
+  // закрываем выпадающие меню на всех уровнях
+  if ($('#menu .menu__link.has-dropdown, #menu .submenu__link.has-dropdown').length) {
     function closeMenu() {
-      $('#menu .has-dropdown').removeClass('dropdown-is-active');
+      console.log('closeMenu() fired!');
+      return $('#menu .has-dropdown').removeClass('dropdown-is-active');
     }
 
-    $('#menu .menu__link.has-dropdown').on('click', function(event) {
-      if ($(window).width() >= 1024) {
-        event.preventDefault();
+    // показываем меню по ховеру
+    $('#menu .menu__link').on('mouseover', function(e) {
+      if ($(window).outerWidth() > 1024) {
+        if (!$(this).hasClass('has-dropdown')) {
+          closeMenu();
+          return;
+        }
 
-        if ($(this).hasClass('dropdown-is-active')) {
-          closeMenu();
-        } else {
-          closeMenu();
-          $(this).addClass('dropdown-is-active');
+        if (!$(this).hasClass('dropdown-is-active')) {
+          const _this = this;
+          $.when(closeMenu()).then(function() {
+            $(_this).addClass('dropdown-is-active');
+          });
         }
       }
     });
 
+    // скрываем, если курсор вышел за пределы хедера
+    $('.sections').on('mouseenter', function(e) {
+      if ($(window).outerWidth() > 1024) {
+        closeMenu();
+      }
+    });
+
+    // скрываем, если кликнули за пределами меню
     $('body').bind('click', function(e) {
       if ($(e.target).closest('#menu .menu__link.has-dropdown').length == 0 && $(e.target).closest('#menu .submenu__link.has-dropdown').length == 0) {
         closeMenu();
       }
     });
 
+    // скрываем, если наэали Esc на клавиатуре
     $(document).keyup(function(e) {
       if (e.keyCode == 27 && $('#menu .menu__link.has-dropdown')) { // escape key maps to keycode `27`
         closeMenu();
@@ -35,18 +51,19 @@ export default () => {
 
   // выпадающее меню второго уровня
   if ($('#menu .submenu__link.has-dropdown').length) {
+    // закрываем выпадающие меню второго уровня
     function closeSubMenu() {
-      $('#menu .submenu__link.has-dropdown').removeClass('dropdown-is-active');
+      console.log('closeSubMenu() fired!');
+      return $('#menu .submenu__link.has-dropdown').removeClass('dropdown-is-active');
     }
 
-    $('#menu .submenu__link.has-dropdown').on('click', function(event) {
-      if ($(window).width() >= 1024) {
-        event.preventDefault();
-
-        if ($(this).hasClass('dropdown-is-active')) {
+    // показываем меню
+    $('#menu .submenu__link').on('mouseover', function(e) {
+      if ($(window).outerWidth() > 1024) {
+        if (!$(this).hasClass('has-dropdown') && $(this).parents('.subsubmenu').length == 0) {
           closeSubMenu();
-        } else {
-          closeSubMenu();
+          return;
+        } else if ($(this).hasClass('has-dropdown')) {
           $(this).addClass('dropdown-is-active');
         }
       }
