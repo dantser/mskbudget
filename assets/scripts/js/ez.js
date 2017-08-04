65f800f82f86072db2702a03b31c378510aa4c4e
Anton Olar <antonolar@mail.ru>
2017-08-04 11:18:08 +0300

    Update 2017-08-04T08:17:16.383Z

--- assets/scripts/js/ez.js
+++ assets/scripts/js/ez.js
@@ -125,6 +125,39 @@ budget.extend("common", {
 
 		},0)
 
+    function appendList(inst, className) {
+        var $selectElement = inst.dpDiv.find('.' + className);
+        var selected = $selectElement.find("option[selected]");
+        var $listElement = $('<dl class="{0} dropdown"></dl>'.replace('{0}', className + '-list')).appendTo($selectElement.parent());
+        $listElement.append('<dt><a href="#">' + selected.text() + '<span class="value">' + selected.val() + '</span></a></dt>');
+        $listElement.append('<dd><ul class="dateListItem" ></ul></dd>');
+        $selectElement.children('option').each(function () {
+            var $listItem = $listElement.find(".dateListItem").append('<li><a href="#">' + $(this).text() + '<span class="value">' + $(this).val() + '</span></a></li>');
+            $listItem.hide();
+            $listItem.find('a').click(function (e) {
+                e.preventDefault();
+                $selectElement.val($(this).find('span').text());
+                $selectElement.change();
+            });
+        });
+        $listElement.find('dt a').click(function (e) {
+            e.preventDefault();
+            $(this).closest('dt')
+              .siblings('dd')
+              .find('.dateListItem')
+              .toggle();
+        });
+    }
+
+    function convertToLists(inst) {
+        var self = this;
+        setTimeout(function () {
+          inst.dpDiv.addClass('datePickerWithLists');
+          appendList(inst, 'ui-datepicker-month');
+          appendList(inst, 'ui-datepicker-year');
+        }, 0);
+    };
+
 		if ($( ".datepicker" ).length) {
 			$( ".datepicker" ).datepicker();
 			$.datepicker.regional['ru'] = {
@@ -143,7 +176,13 @@ budget.extend("common", {
 			dayNamesMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
 			dateFormat: 'dd.mm.yy',
 			firstDay: 1,
-			isRTL: false
+			isRTL: false,
+      beforeShow: function(input, inst) {
+        convertToLists(inst);
+      },
+      onChangeMonthYear: function (year, month, inst) {
+        convertToLists(inst);
+      }
 			};
 			$.datepicker.setDefaults($.datepicker.regional['ru']);
 		}
