d6dbf4e4095bec55338c746ffe3d500856ba575c
Gleb Gorokhov <gg@verstaburo.com>
2017-08-04 20:39:41 +0300

    стрелки для листалок в ie

--- app/components/budgetary-policy/budgetaryPolicy.js
+++ app/components/budgetary-policy/budgetaryPolicy.js
@@ -17,13 +17,31 @@ export default () => {
   // Добавляем стрелочки для IE
   var ua = window.navigator.userAgent;
   var msie = ua.indexOf("MSIE ");
+  if ( msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./) ) {
+    $(document).ready(function(){
+      setTimeout(function(){
+        var arrows = '<div class="ie-arrows"><span class="ie-arrow ie-arrow_left"></span><span class="ie-arrow ie-arrow_right"></span></div>';
+        $(document).find('.carousel-3d-container').append(arrows);
+        $(document).find('.ie-arrows').width( $(document).find('.carousel-3d-slider').outerWidth() );
 
-  if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./))  // If Internet Explorer, return version number
-  {
-      //alert(parseInt(ua.substring(msie + 5, ua.indexOf(".", msie))));
+        $(document).on('click', '.ie-arrow_right', function(){
+          if ( $(document).find('.carousel-3d-slide.current + .carousel-3d-slide').length > 0 ) {
+            $(document).find('.carousel-3d-slide.current + .carousel-3d-slide')[0].click();
+          } else {
+            $(document).find('.carousel-3d-slide:eq(0)')[0].click();
+          }
+        });
+
+        $(document).on('click', '.ie-arrow_left', function(){
+          if ( $(document).find('.carousel-3d-slide.current').prev('.carousel-3d-slide').length > 0 ) {
+            $(document).find('.carousel-3d-slide.current').prev('.carousel-3d-slide')[0].click();
+          } else {
+            $(document).find('.carousel-3d-slide:last')[0].click();
+          }
+        });
+      }, 1000);
+    });
   }
-
-  //alert(msie);
 
   function checkListHeight() {
     $('.mobile-carousel .d-slide__body_list').each(function(){
