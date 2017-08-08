e2bdb5af3ca51f1558a8d57804fc292c9cacc867
qluqua <qluqua@gmail.com>
2017-08-08 17:56:51 +0300

    фикс ДИТ-хедера на главной

--- app/blocks/toppanel/toppanel.js
+++ app/blocks/toppanel/toppanel.js
@@ -1,12 +1,17 @@
 import $ from 'jquery';
 
 export default function toppanel() {
-  const $toppanel = $('.top-panel-frame-wrp');
+  // const $toppanel = $('.top-panel-frame-wrp');
+  const $toppanel = $('body > div:first');
   const $navbar = $('#header');
   const $navbarDrop = $('#headerDrop');
   const $trigger = $('*[data-toggle-toppanel]');
+  const $position = $(window).scrollTop();
+  const hideToppanelOnScrollDown = false;
 
+console.log($toppanel);
   if ($toppanel && $navbar && $trigger) {
+    // Скрыть/показать топпанель ДИТа
     $trigger.on('click', (e) => {
       e.preventDefault();
 
@@ -14,5 +19,19 @@ export default function toppanel() {
       $navbar.toggleClass('toppanel-is-active');
       // $navbarDrop.toggleClass('toppanel-is-active');
     });
+
+    // Скрываем топпанель ДИТ при скролле страницы вниз
+    if (hideToppanelOnScrollDown) {
+      $(window).scroll(function() {
+        var $scroll = $(window).scrollTop();
+        console.log($toppanel.hasClass('is-active'));
+        if ($scroll > $position && $toppanel.hasClass('is-active') && $navbar.hasClass('toppanel-is-active')) {
+          $toppanel.removeClass('is-active');
+          $navbar.removeClass('toppanel-is-active');
+
+          console.log('ska');
+        }
+      });
+    }
   }
 }
