e4b8772dae91ac5478a23b18542c68a70fa950b7
dantser <dantser@gmail.com>
2017-08-16 17:23:24 +0300

    widget fix

--- app/components/depfin-services-main/depfin-services-main.js
+++ app/components/depfin-services-main/depfin-services-main.js
@@ -5,7 +5,8 @@ export default () => {
 
   $( ".sortable" ).sortable({
     items: ".tile__item:not(.tile__item_pinned)",
-    // cancel: ".tile__item_pinned"
+    cancel: ".tile__item_pinned, a, button, input",
+    handle: ".widget-card__head",
   });
   // $( ".tile-item" ).draggable({
   //   connectToSortable: ".tile",
