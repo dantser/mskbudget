fca6fbdb19a17d20d2a47fbdad8ab3548f455d63
Gleb Gorokhov <gg@verstaburo.com>
2017-08-22 12:31:13 +0300

    листалки, фикс скролла

--- gulpfile.js/tasks/templates.js
+++ gulpfile.js/tasks/templates.js
@@ -10,12 +10,8 @@ const gulpIf = require('gulp-if');
 const getData = require('jade-get-data')('app/data');
 
 gulp.task('templates', () => (
-  gulp.src('app/**/*.pug')
-<<<<<<< HEAD
-  //gulp.src('app/pages/budgetary_policy/budgetary_policy.pug')
-=======
-  // gulp.src('app/pages/services_social_significant/services_social_significant.pug')
->>>>>>> eee2a4f7e0bcae677b47883f61371fab107e5ac1
+  //gulp.src('app/**/*.pug')
+  gulp.src('app/pages/principles/principles.pug')
     .pipe(plumber({ errorHandler: errorHandler('Error in templates task') }))
     .pipe(gulpIf(global.isWatching, pugInheritance({ basedir: 'app' })))
     .pipe(filter(file => /app[\\\/]pages/.test(file.path))) // eslint-disable-line no-useless-escape
