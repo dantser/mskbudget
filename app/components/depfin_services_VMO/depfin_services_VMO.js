5f09dfa3fbde57e3a2ee8447d9480439592ff21b
dantser <dantser@gmail.com>
2017-08-24 13:38:56 +0300

    vmo add tab

--- app/components/depfin_services_VMO/depfin_services_VMO.js
+++ app/components/depfin_services_VMO/depfin_services_VMO.js
@@ -18,5 +18,40 @@ export default () => {
     })
   });
 
+// Custom scroll for "паспорта ВМО"
   VMO.find('.services-VMO__table__wrapper').scrollbar();
+
+// переключение таблица-график
+  VMO.find(".analityc-control-button").on("click", function(e) {
+   e.preventDefault();
+   const $this = $(this);
+   const vmo = $(".analityc-widget_VMO"),
+         vmoGraphics = vmo.find($('.services-VMO__graphics')),
+         vmoTable = vmo.find($('.services-VMO__table')),
+         vmoGraphicsActive = vmo.find($('.services-VMO__graphics.active')),
+         vmoTableActive = vmo.find($('.services-VMO__graphics.active')),
+         vmoTableLabel = vmo.find('.services-VMO__label_table'),
+         vmoGraphicsLabel = vmo.find('.services-VMO__label_graphics'),
+         vmoGraphicsLegend = vmo.find('.legend');
+
+   if ($this.hasClass('analityc-control-button_graphics') && !$this.hasClass('active')) {
+     $this.siblings().removeClass('active');
+     $this.addClass('active');
+     vmoTable.removeClass('active');
+     vmoGraphics.addClass('active');
+     vmoTableLabel.hide();
+     vmoGraphicsLabel.show();
+     vmoGraphicsLegend.show();
+
+   } else if ($this.hasClass('analityc-control-button_table') && !$this.hasClass('active')) {
+     $this.siblings().removeClass('active');
+     $this.addClass('active');
+     vmoGraphics.removeClass('active');
+     vmoTable.addClass('active');
+     vmoTableLabel.css('display', 'inline-block');
+     vmoGraphicsLabel.hide();
+     vmoGraphicsLegend.hide();
+   }
+ });
+
 }
