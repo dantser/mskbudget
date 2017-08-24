f2d9bd24b0c35095fdea2ad2f49f6c6fc4e76a06
Ilya <rewasd91@mail.ru>
2017-08-24 10:29:19 +0300

    contest popup

--- app/components/open-con/openCon.js
+++ app/components/open-con/openCon.js
@@ -8,12 +8,12 @@ export default () => {
     const ACTIVE_CLASS = 'button-light--fill';
     EL.on('click', (e) => {
       e.preventDefault();
-      TABLINK.removeClass(ACTIVE_CLASS);
+      EL.parent().siblings().find('.button-light').removeClass(ACTIVE_CLASS);
       EL.addClass(ACTIVE_CLASS);
     })
   })
 
-  $('.button-light').on('click', function(){
+  $('.open-con__con .button-light').on('click', function(){
     $('.analityc-control-switcher_con a').removeClass('active');
     $('.analityc-control-switcher_con a:first-child').addClass('active');
     $('.tabs__tab').hide();
