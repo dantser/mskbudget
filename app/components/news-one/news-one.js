6d7907b6404f062fe78fd0c879396a5e5be52afa
Anton Olar <antonolar@mail.ru>
2017-09-01 11:08:07 +0300

    General fixes

--- app/components/news-one/news-one.js
+++ app/components/news-one/news-one.js
@@ -1,12 +1,19 @@
 import $ from 'jquery';
 
 export default () => {
-  $('.alternative-link__title').on('click', function (e) {
-    e.preventDefault();
-    $('.news').removeClass('news_hidden');
-  })
-  $('.news-one__show-more').on('click', function (e) {
-    e.preventDefault();
-    $('.tile__item:nth-child(n+4)').css('display', 'inline-block');
-  })
+
+	$(document).ready(function(){
+
+	  $('.alternative-link__title').on('click', function (e) {
+	    e.preventDefault();
+	    $('.news').removeClass('news_hidden');
+	  })
+
+	  $('.news-one__show-more').on('click', function (e) {
+	    e.preventDefault();
+			$('.news-one .news-one__card-wrapper').height('auto')
+	  });
+
+	})
+
 }
