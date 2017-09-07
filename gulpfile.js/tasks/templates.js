780134d6759614f5b7bdef2affb374dc19630dfb
Anton Olar <antonolar@mail.ru>
2017-09-07 17:40:12 +0300

    BM: fixes

--- gulpfile.js/tasks/templates.js
+++ gulpfile.js/tasks/templates.js
@@ -11,7 +11,7 @@ const getData = require('jade-get-data')('app/data');
 
 gulp.task('templates', () => (
   gulp.src('app/**/*.pug')
-  // gulp.src(['app/pages/sitemap/sitemap.pug', 'app/pages/depfin-about/depfin-about.pug', 'app/pages/glossary/glossary.pug', ])
+  // gulp.src(['app/pages/budget_income/budget_income.pug', 'app/pages/budget_expenses/budget_expenses.pug', 'app/pages/budget_moscow_sources/budget_moscow_sources.pug'])
     .pipe(plumber({ errorHandler: errorHandler('Error in templates task') }))
     .pipe(gulpIf(global.isWatching, pugInheritance({ basedir: 'app' })))
     .pipe(filter(file => /app[\\\/]pages/.test(file.path))) // eslint-disable-line no-useless-escape
