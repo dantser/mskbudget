1e70718b078905ada0a08c9f57388a0663c02bc6
Ilya <rewasd91@mail.ru>
2017-09-07 18:05:12 +0300

    carousel fixes

--- app/components/budgetary-policy/budgetaryPolicy.js
+++ app/components/budgetary-policy/budgetaryPolicy.js
@@ -58,36 +58,38 @@ export default () => {
     });
   }
 
-  // Добавляем стрелочки для IE
-  //var ua = window.navigator.userAgent;
-  //var msie = ua.indexOf("MSIE ");
-  //if ( msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./) ) {
-  //  var ieVersion = parseInt(ua.substring(msie + 5, ua.indexOf(".", msie)));
-  //  if (ieVersion === 9) {
-  //    $(document).ready(function(){
-  //      setTimeout(function(){
-  //        var arrows = '<div class="ie-arrows"><span class="ie-arrow ie-arrow_left"></span><span class="ie-arrow ie-arrow_right"></span></div>';
-  //        $(document).find('.carousel-3d-container').append(arrows);
-  //        $(document).find('.ie-arrows').width( $(document).find('.carousel-3d-slider').outerWidth() );
-//
-  //        $(document).on('click', '.ie-arrow_right', function(){
-  //          if ( $(document).find('.carousel-3d-slide.current + .carousel-3d-slide').length > 0 ) {
-  //            $(document).find('.carousel-3d-slide.current + .carousel-3d-slide')[0].click();
-  //          } else {
-  //            $(document).find('.carousel-3d-slide:eq(0)')[0].click();
-  //          }
-  //        });
-//
-  //        $(document).on('click', '.ie-arrow_left', function(){
-  //          if ( $(document).find('.carousel-3d-slide.current').prev('.carousel-3d-slide').length > 0 ) {
-  //            $(document).find('.carousel-3d-slide.current').prev('.carousel-3d-slide')[0].click();
-  //          } else {
-  //            $(document).find('.carousel-3d-slide:last')[0].click();
-  //          }
-  //        });
-  //      }, 1000);
-  //    });
-  //  }
-  //}
+  // Стрелочки для IE9
+  var ua = window.navigator.userAgent;
+  var msie = ua.indexOf("MSIE ");
+  if ( msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./) ) {
+    var ieVersion = parseInt(ua.substring(msie + 5, ua.indexOf(".", msie)));
+    if (ieVersion === 9) {
+      $(document).ready(function(){
+        setTimeout(function(){
+          $(arLeft).addClass('ie9');
+          $(arRight).addClass('ie9');
+          //var arrows = '<div class="ie-arrows"><span class="ie-arrow ie-arrow_left"></span><span class="ie-arrow ie-arrow_right"></span></div>';
+          //$(document).find('.carousel-3d-container').append(arrows);
+          //$(document).find('.ie-arrows').width( $(document).find('.carousel-3d-slider').outerWidth() );
+          //
+          //$(document).on('click', '.ie-arrow_right', function(){
+          //  if ( $(document).find('.carousel-3d-slide.current + .carousel-3d-slide').length > 0 ) {
+          //    $(document).find('.carousel-3d-slide.current + .carousel-3d-slide')[0].click();
+          //  } else {
+          //    $(document).find('.carousel-3d-slide:eq(0)')[0].click();
+          //  }
+          //});
+          //
+          //$(document).on('click', '.ie-arrow_left', function(){
+          //  if ( $(document).find('.carousel-3d-slide.current').prev('.carousel-3d-slide').length > 0 ) {
+          //    $(document).find('.carousel-3d-slide.current').prev('.carousel-3d-slide')[0].click();
+          //  } else {
+          //    $(document).find('.carousel-3d-slide:last')[0].click();
+          //  }
+          //});
+        }, 1000);
+      });
+    }
+  }
 
 }
