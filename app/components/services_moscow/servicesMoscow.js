f5dea319f9a244bf3836a1b50c5ea6f329be970d
Ilya <rewasd91@mail.ru>
2017-09-16 16:19:16 +0300

    services moscow

--- app/components/services_moscow/servicesMoscow.js
+++ app/components/services_moscow/servicesMoscow.js
@@ -14,7 +14,7 @@ export default () => {
       loop: true
     });
 
-    var table = new Swiper('.services-moscow-index__index-container', {
+    var table = new Swiper('.services-moscow-index__index', {
       slidesPerView: 5,
       prevButton: '.services-moscow-index__table-arrow_prev',
       nextButton: '.services-moscow-index__table-arrow_next',
@@ -22,11 +22,8 @@ export default () => {
 
       breakpoints: {
         900: {
-          slidesPerView: 3,
-        },
-        580: {
-          slidesPerView: 1,
-        },
+          slidesPerView: 'auto',
+        }
       }
     });
 
@@ -60,4 +57,33 @@ export default () => {
     })
   
     $('.city-popup__wrapper').scrollbar();
+  
+  //Попап
+  $(document).on('click', '.services-moscow-index__row_caption span', function(e){
+    e.stopPropagation();
+    var category = $(this).data('category');
+    var popup = $(this).parents('.services-moscow-index__table').find('.services-moscow-index__city-popup');
+    popup.find('.city-popup__category').removeClass('active');
+    popup.find('.city-popup__category[data-category="'+category+'"]').addClass('active');
+    popup.fadeIn(321);
+  });
+  
+  $(document).on('click', '.services-moscow-params__sources-link', function(e){
+    e.preventDefault();
+    e.stopPropagation();
+    var popup = $(this).parents('.services-moscow__note').find('.services-moscow-params__city-popup');
+    popup.fadeIn(321);
+  });
+  
+  $(document).on('click', '.city-popup__close', function(){
+    $(this).parents('.city-popup').fadeOut(321);
+  });
+  
+  $(document).on('click', '.city-popup', function(e){
+    e.stopPropagation();
+  });
+  
+  $(document).on('click', 'html, body', function(){
+    $('.city-popup').fadeOut(321);
+  });
 }
