02a741ef5f354e8c6dde92a4681b15952b218b20
Ilya <rewasd91@mail.ru>
2017-09-22 19:19:35 +0300

    budget fixes

--- app/components/budget_aip/budget_aip.js
+++ app/components/budget_aip/budget_aip.js
@@ -1,67 +1,48 @@
 import $ from 'jquery';
 
 export default () => {
+  
   const AIP = $('.analityc-widget_aip');
 
   // стрелочки для госпрограмм
-  const ARROW_NEXT = $('.govProgram__arr_next');
-  const ARROW_PREV = $('.govProgram__arr_prev');
-  const selectGP = $('.analityc-widgethead_aip .analityc-control-group_gp .analityc-select');
-  ARROW_NEXT.on('click', function (e) {
+  const selectGP = $('.analityc-widgethead_aip .analityc-control-group_gp select');
+  
+  $('.govProgram__arr').on('click', function(e){  
     e.preventDefault();
-    const govProgram = $('.govProgram');
-    const govProgramActive = $('.govProgram.active');
-    const option = selectGP.find('option');
-    const optionSelected = selectGP.find('option:selected');
-
-    govProgramActive.removeClass('active').next().addClass('active');
-    optionSelected.prop('selected', false).next().prop('selected', true);
-    if (!govProgram.is('.active')) {
-      $('.govProgram:first-child').addClass('active');
+    var gp = $('.govProgram'),
+        currentgp = $('.govProgram.active'),
+        newgp;
+    
+    if ($(this).hasClass('govProgram__arr_next')) {
+      if (currentgp.next().length) {
+        newgp = currentgp.next();
+      } else {
+        newgp = gp.first();
+      }
+    } else {
+      if (currentgp.prev().length) {
+        newgp = currentgp.prev();
+      } else {
+        newgp = gp.last();
+      }
     }
-    if (optionSelected.prop('selected', false).next().length === 0) {
-      selectGP.find('option:first-child').prop('selected', true);
-    }
-  });
-  ARROW_PREV.on('click', function (e) {
-    e.preventDefault();
-    const govProgram = $('.govProgram');
-    const govProgramActive = $('.govProgram.active');
-    const option = selectGP.find('option');
-    const optionSelected = selectGP.find('option:selected');
-
-    govProgramActive.removeClass('active').prev().addClass('active');
-    optionSelected.prop('selected', false).prev().prop('selected', true);
-    if (!govProgram.is('.active')) {
-      $('.govProgram:last-child').addClass('active');
-    }
-    if (optionSelected.prop('selected', false).prev().length === 0) {
-      selectGP.find('option:last-child').prop('selected', true);
-    }
+    
+    var program = newgp.data('program');
+    selectGP.next().find('[data-val="'+program+'"]').click();
   });
 
 
   // изменение селекта "гос программа"
   selectGP.on('change', function () {
-    const $this = $(this);
-    const option = $this.find('option');
-    const govProgram = $('.govProgram');
-    govProgram.removeClass('active');
-    option.removeAttr('selected');
-
-    if($this.val() ===  "Развитие транспортной системы") {
-      govProgram.eq(0).addClass('active');
-      option.eq(0).prop('selected', true);
-    } else if ($this.val() ===  "Столичное здравоохранение") {
-      govProgram.eq(1).addClass('active');
-      option.eq(1).prop('selected', true);
-    } else if ($this.val() ===  "Социальная поддержка жителей Москвы") {
-      govProgram.eq(2).addClass('active');
-      option.eq(2).prop('selected', true);
-    } else if ($this.val() ===  "Столичное образование") {
-      govProgram.eq(3).addClass('active');
-      option.eq(3).prop('selected', true);
-    }
+    var programVal = $(this).val(),
+        gpblock = $('.govProgram');
+    gpblock.removeClass('active');
+    gpblock.each(function(){
+      var program = $(this).data('program');
+      if (program.match(programVal)) {
+        $(this).addClass('active');
+      }
+    });
   });
 
 
