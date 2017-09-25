31ea3f8a989c95c03b2ab9b0752984cfd7922a81
Gleb Gorokhov <gg@verstaburo.com>
2017-09-25 17:25:59 +0300

    gov realisation fixes

--- app/blocks/slide-arrows/slideArrows.js
+++ app/blocks/slide-arrows/slideArrows.js
@@ -1,27 +1,27 @@
 import $ from 'jquery';
 
 export default () => {
-  
+
   function slideArrow(el, arrows, offset) {
-    
+
     if (el.length > 0) {
-      
+
       arrows.find('.slide-arrows__arrow_left').on('click', function (e) {
         if (offset === 'sliderwidth') {offset = el.outerWidth();}
         el.animate( { scrollLeft: '-='+offset }, 300);
       });
-    
+
       arrows.find('.slide-arrows__arrow_right').on('click', function (e) {
         if (offset === 'sliderwidth') {offset = el.outerWidth();}
         el.animate( { scrollLeft: '+='+offset }, 300);
       });
-      
+
       function arrowActive(el) {
-          
+
         var sL = el.scrollLeft(),
             leftArr = arrows.find('.slide-arrows__arrow_left'),
             rightArr = arrows.find('.slide-arrows__arrow_right');
-      
+
         if (sL === 0) {
           leftArr.addClass('disabled');
         } else if (sL === ( el.prop('scrollWidth') - el.width().toFixed(0) ) ) {
@@ -31,29 +31,29 @@ export default () => {
           rightArr.removeClass('disabled');
         }
       }
-      
+
       arrowActive(el);
       el.scroll(function(){
         arrowActive($(this));
       });
-      
+
     }
   }
-  
+
   // Инициализация
   if ($('.slide-arrows').length) {
     $('.slide-arrows').each(function(){
-      var container = $(this).parents('.analityc-table, .linear-diagrams, .analityc-graphics'),
-          wrapper = container.find('.analityc-table__wrapper, .linear-diagrams__wrapper, .analityc-graphics__graphic-slider, .budget-forecast__table-wrapper'),
+      var container = $(this).parents('.analityc-table, .linear-diagrams, .analityc-graphics, .dropdown__content'),
+          wrapper = container.find('.analityc-table__wrapper, .linear-diagrams__wrapper, .analityc-graphics__graphic-slider, .budget-forecast__table-wrapper, .table-wrapper'),
           arrows = $(this),
           offset = wrapper.data('offset');
       console.log(wrapper);
       slideArrow(wrapper, arrows, offset);
     });
   }
-  
+
   //// Аналитика - Государственный долг
   //slideArrow('.analytics-gov-debt__graphic-wrapper', '.analytics-gov-debt__slide-arrows_graphics', 100);
   //slideArrow('.analytics-gov-debt__table-wrapper', '.analytics-gov-debt__slide-arrows_table', 100);
-  
+
 }
