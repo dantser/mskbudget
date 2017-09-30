02ae3929994c1574283a28e246ca29491e69977c
Anton Olar <antonolar@mail.ru>
2017-09-30 13:58:48 +0300

    Media-materials: fixes

--- app/components/depfin-budget-moscow-media-materials/mediaMaterials.js
+++ app/components/depfin-budget-moscow-media-materials/mediaMaterials.js
@@ -16,6 +16,8 @@ export default () => {
   const sliderGallery = new Swiper(page+ '__materials_slider ' +page+ '__slider-gallery', {
     nextButton: page+ '__slider-next',
     prevButton: page+ '__slider-prev',
+    pagination: page+ '__paginate-bullets',
+    paginationClickable: true
   });
   const sliderPagination = new Swiper(page+ '__materials_slider ' +page+ '__slider-pagination', {
     spaceBetween: 35,
@@ -23,6 +25,11 @@ export default () => {
     centeredSlides: true,
     touchRatio: 0.2,
     slideToClickedSlide: true,
+    breakpoints: {
+      900: {
+        slidesPerView: '2',
+      }
+    }
   });
   sliderGallery.params.control = sliderPagination;
   sliderPagination.params.control = sliderGallery;
@@ -35,5 +42,35 @@ export default () => {
     popup.fadeIn(321);
   });
 
+  function overflowDotts(size, element) {
+    var content = $(element);
+    content.each(function () {
+      var contentText = $(this).text();
+      if(contentText.length > size){
+        $(this).text(contentText.slice(0, size) + ' ...');
+      }
+    });
+  };
 
+  const containerWidth = $(document).width();
+
+  if (containerWidth < 1140 && containerWidth >= 401)
+    overflowDotts(42, page + '__text'); 
+  else if (containerWidth < 400)
+    overflowDotts(80, page + '__text'); 
+  else
+    overflowDotts(60, page + '__text'); 
+
+
+  const moreBtn = $(page + '__more button');
+  const contentWrapper = $(page + '__wrapper');
+
+  moreBtn.on('click', function(e) {
+    e.preventDefault();
+    contentWrapper.animate({
+      height: contentWrapper.find(page + '__content').height()
+    }, 1000, function(){
+      contentWrapper.height('auto');
+    });
+  })
 }
