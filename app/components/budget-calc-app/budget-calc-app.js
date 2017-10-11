4b46c9ebb8e28c07aa125172388ff14be4b77dbe
Ilya <rewasd91@mail.ru>
2017-10-11 12:23:56 +0300

    budget calc fix

--- app/components/budget-calc-app/budget-calc-app.js
+++ app/components/budget-calc-app/budget-calc-app.js
@@ -149,12 +149,5 @@ export default () => {
     window.addEventListener('load', moveAroundThoseWidgets);
     window.addEventListener('resize', moveAroundThoseWidgets);
   }
-  
-  
-  
-  $('.budget-calc-app .range-ui__input').click(function(e){
-    e.preventDefault();
-    e.stopPropagation();
-    $(this).focus();
-  });
+
 }
