2cc087b74589427db8104555509972de239a4364
Alexandr Kucherenko <alexanderk@sweetsoft.com.ua>
2017-11-22 14:51:45 +0200

    Slider

--- app/components/depfin-budget-moscow-open-reit/openReit.js
+++ app/components/depfin-budget-moscow-open-reit/openReit.js
@@ -3,6 +3,9 @@ import $ from 'jquery';
 export default () => {
   $('.select-element1').selectmenu().selectmenu( "menuWidget" ).addClass( "ui-openreit" );
   $('.select-element2').selectmenu().selectmenu( "menuWidget" ).addClass( "ui-openreit" );
-  $('.analityc-select_transparentyellow').selectmenu().selectmenu( "menuWidget" ).addClass( "ui-openfed" );
-
+  $('.analityc-select_transparentyellow').selectmenu({
+  	change: function( event, ui ) {
+  		$('.analityc-select_transparentyellow').trigger("change");
+	}
+  }).selectmenu( "menuWidget" ).addClass( "ui-openfed" );
 }
