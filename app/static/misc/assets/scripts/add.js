bb88fa2fe529ad1bbe815c740ba090bf31b2ad85
Ilya <rewasd91@mail.ru>
2017-11-29 15:45:23 +0300

    mrelations

--- app/static/misc/assets/scripts/add.js
+++ app/static/misc/assets/scripts/add.js
@@ -71,98 +71,113 @@ $(document).on('click', 'html, body', function(){
 // Custom Selects jQuery UI
 $(document).ready(function(){
   
-  $('.select-element1').selectmenu({
-    change: function(event, ui) {
-      var val = $(this).val();
-      $(this).find('option').removeAttr('selected');
-      $(this).find('option').each(function(){
-        if ($(this).val() == val) {
-          $(this).attr('selected', 'selected');
-        }
-      });
-      $(this).change();
-    }
-  }).selectmenu( "menuWidget" ).addClass( "ui-openreit" );
+  if ($('.select-element1').length) {
+    
+    $('.select-element1').selectmenu({
+      change: function(event, ui) {
+        var val = $(this).val();
+        $(this).find('option').removeAttr('selected');
+        $(this).find('option').each(function(){
+          if ($(this).val() == val) {
+            $(this).attr('selected', 'selected');
+          }
+        });
+        $(this).change();
+      }
+    }).selectmenu( "menuWidget" ).addClass( "ui-openreit" );
+    
+  }
   
-  $('.select-element2').selectmenu({
-    change: function(event, ui) {
-      var val = $(this).val();
-      $(this).find('option').removeAttr('selected');
-      $(this).find('option').each(function(){
-        if ($(this).val() == val) {
-          $(this).attr('selected', 'selected');
-        }
-      });
-      $(this).change();
-    }
-  }).selectmenu( "menuWidget" ).addClass( "ui-openreit" );
+  if ($('.select-element2').length) {
+    
+    $('.select-element2').selectmenu({
+      change: function(event, ui) {
+        var val = $(this).val();
+        $(this).find('option').removeAttr('selected');
+        $(this).find('option').each(function(){
+          if ($(this).val() == val) {
+            $(this).attr('selected', 'selected');
+          }
+        });
+        $(this).change();
+      }
+    }).selectmenu( "menuWidget" ).addClass( "ui-openreit" );
+    
+  }
+  
 });
 
 
 
 // Inputmasks
 $(document).ready(function(){
- 
-  window.inputMasks = function() {
-    Inputmask({
-      mask: '99.99.9999',
-      clearMaskOnLostFocus: false,
-      positionCaretOnClick: 'none',
-      onincomplete: function(){
-        $(this).siblings('.contest-popup__label').addClass('contest-popup__err')
-      }
-    }).mask('.contest-popup__fio#pi-birth');
+  
+  if ($('.contest-popup').length) {
     
-    Inputmask({
-      mask: '+7 (999) 999-9999',
-      clearMaskOnLostFocus: false,
-      positionCaretOnClick: 'none',
-      onincomplete: function(){
-        $(this).siblings('.contest-popup__label').addClass('contest-popup__err')
-      }
-    }).mask('.contest-popup__fio#pi-phone, .contest-popup__fio#pj-phone');
+    window.inputMasks = function() {
+      
+      Inputmask({
+        mask: '99.99.9999',
+        clearMaskOnLostFocus: false,
+        positionCaretOnClick: 'none',
+        onincomplete: function(){
+          $(this).siblings('.contest-popup__label').addClass('contest-popup__err')
+        }
+      }).mask('.contest-popup__fio#pi-birth');
+      
+      Inputmask({
+        mask: '+7 (999) 999-9999',
+        clearMaskOnLostFocus: false,
+        positionCaretOnClick: 'none',
+        onincomplete: function(){
+          $(this).siblings('.contest-popup__label').addClass('contest-popup__err')
+        }
+      }).mask('.contest-popup__fio#pi-phone, .contest-popup__fio#pj-phone');
+      
+      //Inputmask({
+      //  mask: "*{1,80}@i{1,20}.i{1,6}[.i{1,2}]",
+      //  clearMaskOnLostFocus: false,
+      //  positionCaretOnClick: 'none',
+      //  greedy: false,
+      //  onBeforePaste: function (pastedValue, opts) {
+      //    pastedValue = pastedValue.toLowerCase();
+      //    return pastedValue.replace("mailto:", "");
+      //  },
+      //  definitions: {
+      //    '*': {
+      //      validator: "[0-9A-Za-z._-]",
+      //      cardinality: 1,
+      //      casing: "lower"
+      //    },
+      //    'i': {
+      //      validator: "[0-9A-Za-z_-]",
+      //      cardinality: 1,
+      //      casing: "lower"
+      //    }
+      //  }
+      //}).mask('.contest-popup__fio#pi-email, .contest-popup__fio#pj-email');
+      
+      //Inputmask({
+      //  mask: "*{1,80}",
+      //  placeholder: "",
+      //  clearMaskOnLostFocus: false,
+      //  positionCaretOnClick: 'none',
+      //  onBeforePaste: function (pastedValue, opts) {
+      //    pastedValue = pastedValue.toLowerCase();
+      //    return pastedValue.replace("mailto:", "");
+      //  },
+      //  definitions: {
+      //    '*': {
+      //      validator: "[0-9A-Za-z@._-]",
+      //      cardinality: 1,
+      //      casing: "lower"
+      //    }
+      //  }
+      //}).mask('.contest-popup__fio#pi-email, .contest-popup__fio#pj-email');
+    }
     
-    //Inputmask({
-    //  mask: "*{1,80}@i{1,20}.i{1,6}[.i{1,2}]",
-    //  clearMaskOnLostFocus: false,
-    //  positionCaretOnClick: 'none',
-    //  greedy: false,
-    //  onBeforePaste: function (pastedValue, opts) {
-    //    pastedValue = pastedValue.toLowerCase();
-    //    return pastedValue.replace("mailto:", "");
-    //  },
-    //  definitions: {
-    //    '*': {
-    //      validator: "[0-9A-Za-z._-]",
-    //      cardinality: 1,
-    //      casing: "lower"
-    //    },
-    //    'i': {
-    //      validator: "[0-9A-Za-z_-]",
-    //      cardinality: 1,
-    //      casing: "lower"
-    //    }
-    //  }
-    //}).mask('.contest-popup__fio#pi-email, .contest-popup__fio#pj-email');
+    inputMasks();
     
-    //Inputmask({
-    //  mask: "*{1,80}",
-    //  placeholder: "",
-    //  clearMaskOnLostFocus: false,
-    //  positionCaretOnClick: 'none',
-    //  onBeforePaste: function (pastedValue, opts) {
-    //    pastedValue = pastedValue.toLowerCase();
-    //    return pastedValue.replace("mailto:", "");
-    //  },
-    //  definitions: {
-    //    '*': {
-    //      validator: "[0-9A-Za-z@._-]",
-    //      cardinality: 1,
-    //      casing: "lower"
-    //    }
-    //  }
-    //}).mask('.contest-popup__fio#pi-email, .contest-popup__fio#pj-email');
   }
   
-  inputMasks();
 });
