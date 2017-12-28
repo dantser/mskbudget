d31d43266d30e1e471ec9523357c17af050bcc41
Ilya <rewasd91@mail.ru>
2017-12-28 21:08:37 +0300

    Update 2017-12-28T18:08:03.443Z

--- assets/scripts/add.js
+++ assets/scripts/add.js
@@ -486,3 +486,27 @@ function cardSliderInit() {
   cardSlider('.section-tabs_news');
   cardSlider('.section-tabs_services');
 }
+
+
+
+// checkMobile
+var checkMobile = {
+  Android: function() {
+    return navigator.userAgent.match(/Android/i);
+  },
+  BlackBerry: function() {
+    return navigator.userAgent.match(/BlackBerry/i);
+  },
+  iOS: function() {
+    return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+  },
+  Opera: function() {
+    return navigator.userAgent.match(/Opera Mini/i);
+  },
+  Windows: function() {
+    return navigator.userAgent.match(/IEMobile/i);
+  },
+  any: function() {
+    return (checkMobile.Android() || checkMobile.BlackBerry() || checkMobile.iOS() || checkMobile.Opera() || checkMobile.Windows());
+  }
+};
